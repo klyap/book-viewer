1b6626edf2de3a0b27507e59628754bb2fd88a5c
Ker Lee Yap <kl.yap@hotmail.com>
2022-03-13 22:33:28 -0700

    cleaning up

--- src/pages/ViewPage.jsx
+++ src/pages/ViewPage.jsx
@@ -4,7 +4,6 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import { useState } from 'react';
 import { getDownloadURL, ref, getStorage } from "@firebase/storage";
 
-// var url = "https://firebasestorage.googleapis.com/v0/b/book-viewer-87150.appspot.com/o/1646965043148?alt=media&token=ba7a30a0-28d2-49d3-a794-f1e793acb55c";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export const ViewPage = () => {
